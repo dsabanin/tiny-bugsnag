f79534ccee5514cdf6982ef5500514717c718ca9
Dima Sabanin <sdmitry@gmail.com>
2014-10-29 13:46:04 -0400

    added description, homepage link and bumped version to 0.1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tiny-bugsnag "0.1.3"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tiny-bugsnag "0.1.4"
+  :description "A tiny Clojure client for bugsnag.com"
+  :url "https://github.com/dsabanin/tiny-bugsnag"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
